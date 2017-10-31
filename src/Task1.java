ea0e8b721eff7ec06b252469e7015512f1732665
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-10-31 13:15:36 +0300

    Completed better Task1

--- src/Task1.java
+++ src/Task1.java
@@ -3,14 +3,14 @@ public class Task1 {
 
         String[] LetterList = {"a","b","c","d","e"};
         String[] NumberList = {"0","2","4","6","8"};
-        String result;
+        String result = "";
 
         int x = 0;
         do {
-            result = LetterList[x].concat(NumberList[x]);
+            result += LetterList[x].concat(NumberList[x]);
             x++;
-            System.out.print(result);
         } while (x<=4);
 
+        System.out.println(result);
     }
 }
