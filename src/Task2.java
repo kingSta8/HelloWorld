81962c53153290f0a45e565168a6e2fe4bdb69c5
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-01 11:49:55 +0300

    Completed Task2

--- src/Task2.java
+++ src/Task2.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
         System.out.print("Введите строку: ");
         String string = in.nextLine();
-
+        String res = string.replaceAll("[0-9]","");
+        System.out.println("Ваша строка без цифр: "+res);
+        }
     }
-}
