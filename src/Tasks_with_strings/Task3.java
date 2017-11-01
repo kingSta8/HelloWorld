15afc10bff9a0748a1ebf5887b6906887eff6d0f
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-01 12:21:09 +0300

    Completed Task3

--- src/Tasks_with_strings/Task3.java
+++ src/Tasks_with_strings/Task3.java
@@ -1,4 +1,13 @@
 package Tasks_with_strings;
 
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String string = in.nextLine();
+        String res = string.replaceAll("^\\s+|\\s+|\\s+$", " ");
+        System.out.println("Ваша строка без подряд идущих пробелов: "+res);
+    }
 }
