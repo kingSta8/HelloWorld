c532fec16d3841e456fc973b053b1205d542e340
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-09 13:05:06 +0300

    Completed better Task2

--- src/Tasks_with_strings/Task1.java
+++ src/Tasks_with_strings/Task1.java
@@ -2,16 +2,15 @@ package Tasks_with_strings;
 
 public class Task1 {
     public static void main(String[] args) {
-
         String[] LetterList = {"a","b","c","d","e"};
         String[] NumberList = {"0","2","4","6","8"};
         String result = "";
 
         int x = 0;
-        do {
-            result += LetterList[x].concat(NumberList[x]);
-            x++;
-        } while (x<=4);
+            do {
+                result += LetterList[x].concat(NumberList[x]);
+                x++;
+            } while (x<=4);
 
         System.out.println(result);
     }
--- src/Tasks_with_strings/Task2.java
+++ src/Tasks_with_strings/Task2.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
+        System.out.println("Здравствуйте! Данная программа удаляет все цифры из строки.");
+        String answer;
+
+        do {
         System.out.print("Введите строку: ");
         String string = in.nextLine();
         String res = string.replaceAll("[0-9]","");
         System.out.println("Ваша строка без цифр: "+res);
+            System.out.print("Выполнить программу снова? (да/нет) -->  ");
+            answer = in.nextLine();
+        } while (answer.equals("да"));
+        System.out.println("До свидания! Хорошего дня!");
         }
     }
