13aebeeed79bf78adc66b504bafc105649c713a4
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-09 13:07:43 +0300

    Completed better Task3

--- src/Tasks_with_strings/Task3.java
+++ src/Tasks_with_strings/Task3.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+        System.out.println("Здравствуйте! Данная программа меняет подряд идущие пробелы на один пробел.");
+        String answer;
+
+        do {
         System.out.print("Введите строку: ");
         String string = in.nextLine();
         String res = string.replaceAll("^\\s+|\\s+|\\s+$", " ");
         System.out.println("Ваша строка без подряд идущих пробелов: "+res);
+            System.out.print("Выполнить программу снова? (да/нет) -->  ");
+            answer = in.nextLine();
+        } while (answer.equals("да"));
+        System.out.println("До свидания! Хорошего дня!");
     }
 }
