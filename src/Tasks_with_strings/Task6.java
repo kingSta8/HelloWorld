0dc47321d8d8c7e748f640d305f52fd5b255f97a
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-13 14:20:32 +0300

    Completed Task6

--- src/Tasks_with_strings/Task6.java
+++ src/Tasks_with_strings/Task6.java
@@ -1,55 +1,54 @@
 package Tasks_with_strings;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Здравствуйте! Данная программа меняет местами наибольшее и наименьшее по длине слова в веденной Вами строке.");
-        System.out.print("Ваша строка: ");
-        String string = in.nextLine();
-        String[] mas = string.split(" ");
+        String answer;
 
-        int posmax = 0, posmin = 0;
+        do {
+            System.out.print("Введите строку: ");
+            String str = in.nextLine();
+            System.out.println(getStringWithReplaceMinMaxWord(str));
+            System.out.print("Выполнить программу снова? (да/нет) -->  ");
+            answer = in.nextLine();
+        } while (answer.equals("да"));
+        System.out.println("До свидания! Хорошего дня!");
+    }
 
-        String[] chAr1 = string.split(" ");
-        String max = chAr1[0];
-        for(int i=1;i<chAr1.length;i++){
-            if(max.length()<chAr1[i].length())
-                max = chAr1[i];
-                posmax = i;
+
+    public static String getStringWithReplaceMinMaxWord(String str) {
+
+        String stringBuilderMaxWord = new String();
+        String stringBuilderMinWord = new String(String.valueOf(Integer.MAX_VALUE));
+        int countMax = 0;
+        int countMin = 0;
+        int count = 0;
+
+        for (String s : str.split(" ")) {
+            if (stringBuilderMaxWord.length() < s.length()) {
+                stringBuilderMaxWord = s;
+                countMax = count + 1;
+            }
+
+            if (stringBuilderMinWord.length() > s.length()) {
+                stringBuilderMinWord = s;
+                countMin = count + 1;
+            }
+            count++;
         }
 
+        if (countMax > countMin) {
+            str = str.replaceFirst(stringBuilderMaxWord, stringBuilderMinWord);
+            str = str.replaceFirst(stringBuilderMinWord, stringBuilderMaxWord);
+        } else {
+            str = str.replaceFirst(stringBuilderMinWord, stringBuilderMaxWord);
+            str = str.replaceFirst(stringBuilderMaxWord, stringBuilderMinWord);
 
-        String[] chAr2 = string.split(" ");
-        String min = chAr2[0];
-        for(int j=1;j<chAr2.length;j++){
-            if(min.length()>chAr2[j].length())
-                min = chAr2[j];
-                posmin = j;
         }
 
-        /*String res = string.replace('posmax','posmin');*/
-
-
-
-        /*String res = "";
-        String[] sentenses = string.split("(?<=[.!?])");
-        for (String str : sentenses) {
-            String newSentense = str.trim().replaceAll("(?U)^(\\w+)(.*)(\\b\\w+)([.?!]?$)", "$3$2$1$4");
-            System.out.println(newSentense);
-        }*/
-
-       /* mas[posmax] = min;
-        mas[posmin] = max;
-        System.out.println ("Полученная строка: ");
-        System.out.println (Arrays.toString(mas));*/
-
-       /* System.out.println("Ваша строка с заменой: "+res);*/
-        /*String[] words = string.replaceAll("^\\s+|\\s+|\\s+$", " ").split("\\s");
-        for(String subStr:words) {
-            System.out.println(subStr);
-        }*/
+        return str;
     }
 }
