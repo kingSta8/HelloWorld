e54fff52ce6685d8c88907b42d82d9b56be274d4
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-14 12:27:04 +0300

    Task6 with messages

--- src/Tasks_with_strings/Task7.java
+++ src/Tasks_with_strings/Task7.java
@@ -3,13 +3,21 @@ package Tasks_with_strings;
 import java.util.Scanner;
 
 public class Task7 {
+
+    public static final String EXPLANATION_MESSAGE = "предназначена для определения правильности расстановки скобок в выражении.";
+    public static final String ENTER_EXPRESSION = "Введите математическое выражение: ";
+    public static final String BKT_IN_EXPRESSION = "Скобки в выражениии ";
+    public static final String INCORRECT = " расставлены неправильно.";
+    public static final String CORRECT = " расставлены правильно.";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Здравствуйте! Данная программа предназначена для определения правильности расстановки скобок в выражении.");
+        System.out.print(Constants.HELLO_MESSAGE);
+        System.out.println(EXPLANATION_MESSAGE);
         String answer;
 
         do {
-            System.out.print("Введите математическое выражение: ");
+            System.out.print(ENTER_EXPRESSION);
             String expr = in.nextLine();
             int k = 0;
             for (char ch : expr.toCharArray()) {
@@ -19,13 +27,13 @@ public class Task7 {
 
             }
             if (k != 0) {
-                System.out.println("Скобки в выражениии " + expr + " расставлены неправильно.");
+                System.out.println(BKT_IN_EXPRESSION + expr + INCORRECT);
             } else {
-                System.out.println("Скобки в выражении " + expr + " расставлены правильно.");
+                System.out.println(BKT_IN_EXPRESSION + expr + CORRECT);
             }
-            System.out.print("Выполнить программу снова? (да/нет) -->  ");
+            System.out.print(Constants.REPEAT_MESSAGE);
             answer = in.nextLine();
-        } while (answer.equals("да"));
-        System.out.println("До свидания! Хорошего дня!");
+        } while (answer.equals(Constants.YES));
+        System.out.println(Constants.GOOD_BYE_MESSAGE);
     }
 }
