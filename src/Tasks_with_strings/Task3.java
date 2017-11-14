767d49bf0013cbdf4ae2fa4d0010748d444b1cf1
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-14 12:06:19 +0300

    Task3 with messages

--- src/Tasks_with_strings/Task3.java
+++ src/Tasks_with_strings/Task3.java
@@ -3,19 +3,24 @@ package Tasks_with_strings;
 import java.util.Scanner;
 
 public class Task3 {
+
+    public static final String EXPLANATION_MESSAGE = "меняет подряд идущие пробелы на один пробел.";
+    public static final String RESULT_MESSAGE = "Ваша строка без подряд идущих пробелов: ";
+
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-        System.out.println("Здравствуйте! Данная программа меняет подряд идущие пробелы на один пробел.");
+        System.out.print(Constants.HELLO_MESSAGE);
+        System.out.println(EXPLANATION_MESSAGE);
         String answer;
 
         do {
-        System.out.print("Введите строку: ");
+        System.out.print(Constants.ENTER_STRING);
         String string = in.nextLine();
         String res = string.replaceAll("^\\s+|\\s+|\\s+$", " ");
-        System.out.println("Ваша строка без подряд идущих пробелов: "+res);
-            System.out.print("Выполнить программу снова? (да/нет) -->  ");
+        System.out.println(RESULT_MESSAGE +res);
+            System.out.print(Constants.REPEAT_MESSAGE);
             answer = in.nextLine();
-        } while (answer.equals("да"));
-        System.out.println("До свидания! Хорошего дня!");
+        } while (answer.equals(Constants.YES));
+        System.out.println(Constants.GOOD_BYE_MESSAGE);
     }
 }
