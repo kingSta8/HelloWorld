5bc230ef7531d7b251b27fb834964bc3b07af324
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-14 12:47:58 +0300

    Task8 with messages better

--- src/Tasks_with_strings/Task8.java
+++ src/Tasks_with_strings/Task8.java
@@ -8,12 +8,13 @@ public class Task8 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(Constants.HELLO_MESSAGE);
+        System.out.print(Constants.HELLO_MESSAGE);
         System.out.println(EXPLANATION_MESSAGE);
         String answer;
         do {System.out.print(Constants.ENTER_STRING);
             String str = in.nextLine();
             String res = str.replaceAll("A","_a").replaceAll("B","_b").replaceAll("C","_c").replaceAll("D","_d").replaceAll("E","_e").replaceAll("F","_f").replaceAll("G","_g").replaceAll("H","_h").replaceAll("I","_i").replaceAll("J","_j").replaceAll("K","_k").replaceAll("L","_l").replaceAll("M","_m").replaceAll("N","_n").replaceAll("O","_o").replaceAll("P","_p").replaceAll("Q","_q").replaceAll("R","_r").replaceAll("S","_s").replaceAll("T","_t").replaceAll("U","_u").replaceAll("V","_v").replaceAll("W","_w").replaceAll("X","_x").replaceAll("Y","_y").replaceAll("Z","_z");
+            System.out.println(65);
             System.out.println(RESULT_MESSAGE +res);
             System.out.print(Constants.REPEAT_MESSAGE);
             answer = in.nextLine();
