b1e023652a1e34ca04cf599e45d13c37a6eb2346
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-15 14:33:51 +0300

    Smth for Task2

---
+++ src/Tasks_with_collections/Task2.java
@@ -0,0 +1,16 @@
+package Tasks_with_collections;
+
+import Tasks_with_strings.Constants;
+
+public class Task2 {
+
+    public static final String EXPLANATION_MESSAGE = "сравнивает 2 объекта по количеству элементов в списке.";
+
+    public static void main(String[] args) {
+        System.out.println(Constants.HELLO_MESSAGE);
+        System.out.println(EXPLANATION_MESSAGE);
+
+
+        System.out.println(Constants.GOOD_BYE_MESSAGE);
+    }
+}
