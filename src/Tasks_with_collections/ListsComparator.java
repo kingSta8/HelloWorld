d78c1108a0a9e8f76a951d3a73909db25f94a17f
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-15 17:17:26 +0300

    Smth for Task2

---
+++ src/Tasks_with_collections/ListsComparator.java
@@ -0,0 +1,10 @@
+package Tasks_with_collections;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public interface ListsComparator extends Comparator {
+    public LinkedList<String> list1 = new LinkedList<String>();
+    public static LinkedList<String> list2 = new LinkedList<String>();
+    /*public int compare(Object list1, Object list2);*/
+}
--- src/Tasks_with_collections/Task2.java
+++ src/Tasks_with_collections/Task2.java
@@ -1,7 +1,6 @@
 package Tasks_with_collections;
 
 import Tasks_with_strings.Constants;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Task2 {
@@ -9,12 +8,7 @@ public class Task2 {
     private static final String ENTER_FIRST_LIST = "Введите первый список: ";
     private static final String ENTER_SECOND_LIST = "Введите второй список: ";
     private static final String EXPLANATION_MESSAGE = "сравнивает 2 списка по количеству элементов в списке.";
-    private static LinkedList<String> list1 = new LinkedList<String>();
-    private static LinkedList<String> list2 = new LinkedList<String>();
 
-    private interface ListsComparator {
-
-    }
 
     public static void main(String[] args) {
         System.out.print(Constants.HELLO_MESSAGE);
@@ -26,9 +20,9 @@ public class Task2 {
 
         do {
             System.out.print(ENTER_FIRST_LIST);
-            list1.add(in.nextLine());
+            ListsComparator.list1.add(in.nextLine());
             System.out.print(ENTER_SECOND_LIST);
-            list2.add(in.nextLine());
+            ListsComparator.list2.add(in.nextLine());
 
 
 
