b6a8fad98303b8f17dd6443978e96e0189e84d4d
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-15 14:29:34 +0300

    Smth for Task1

---
+++ src/Tasks_with_collections/Task1.java
@@ -0,0 +1,16 @@
+package Tasks_with_collections;
+
+import Tasks_with_strings.Constants;
+
+public class Task1 {
+
+    public static final String EXPLANATION_MESSAGE = "сравнивает работу различных коллекций.";
+
+    public static void main(String[] args) {
+        System.out.println(Constants.HELLO_MESSAGE);
+        System.out.println(EXPLANATION_MESSAGE);
+
+
+        System.out.println(Constants.GOOD_BYE_MESSAGE);
+    }
+}
