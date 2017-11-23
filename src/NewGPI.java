35d05026f3be102582edcfc00860f39ee70b4cf6
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-23 17:07:24 +0300

    smth for NewGPI

--- src/NewGPI.java
+++ src/NewGPI.java
@@ -1,13 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class NewGPI {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setTitle("Hello,mthrfckr! program");
+        frame.setSize(1920, 1080);
+
         JButton button = new JButton("Hello, mthrfckr!");
         frame.getContentPane().add(button);
-        frame.setSize(1920, 1080);
-        frame.setVisible(true);
 
+        frame.setVisible(true);
 
     }
 
