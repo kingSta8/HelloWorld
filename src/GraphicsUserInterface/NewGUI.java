580aa877c308a2e55d9ebc58728ece984181bf84
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-28 14:04:55 +0300

    smth for NewGUI

--- src/GraphicsUserInterface/NewGUI.java
+++ src/GraphicsUserInterface/NewGUI.java
@@ -2,24 +2,55 @@ package GraphicsUserInterface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class NewGUI {
+public class NewGUI extends JFrame {
+
+    private JButton button = new JButton("Далее");
+    private JLabel label1 = new JLabel("Введите ваше имя:");
+    private JLabel label2 = new JLabel("Кто вы?");
+    private JTextField input = new JTextField("", 5);
+    private JRadioButton radio1 = new JRadioButton("дебил");
+    private JRadioButton radio2 = new JRadioButton("не дебил");
+
+   public NewGUI() {
+       super("Тест 'Познай себя'");
+       this.setBounds(100, 100, 500, 200);
+       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       Container container = this.getContentPane();
+        container.setLayout(new GridLayout(3,2,2,2));
+        container.add(label1);
+        container.add(input);
+
+        container.add(label2);
+       ButtonGroup group = new ButtonGroup();
+       group.add(radio1);
+       group.add(radio2);
+        container.add(radio1);
+       radio1.setSelected(true);
+       container.add(radio2);
+
+       button.addActionListener(new ButtonEventListener());
+       container.add(button);
+    }
+
+    class ButtonEventListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String message = "";
+            message += "Поздравляю, " + input.getText() + ", вы ";
+            message +=  ((radio1.isSelected())
+                    ? "дебил" : "не дебил" );
+            message += " !";
+            JOptionPane.showMessageDialog(null,
+                    message,
+                    "Результат",
+                    JOptionPane.PLAIN_MESSAGE);
+        }
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("Hello,mthrfckr! program");
-        frame.setSize(1080, 720);
-
-        JButton button = new JButton("Hello, mthrfckr!");
-        JButton button2 = new JButton("U r ccksckr");
-        JButton button3 = new JButton("Fck u");
-        JButton button4 = new JButton("G0 fck urslf");
-        /*button4.addActionListener();*/
-        frame.getContentPane().add(BorderLayout.WEST,button);
-        frame.getContentPane().add(BorderLayout.EAST,button2);
-        frame.getContentPane().add(BorderLayout.NORTH,button3);
-        frame.getContentPane().add(BorderLayout.SOUTH,button4);
-
-        frame.setVisible(true);
-
+        NewGUI test = new NewGUI();
+        test.setVisible(true);
     }
 }
