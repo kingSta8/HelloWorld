4d76b35c2944ce728d26be480c6c88973626f497
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2017-11-29 17:05:55 +0300

    TEST version1

--- src/GraphicsUserInterface/NewGUI.java
+++ src/GraphicsUserInterface/NewGUI.java
@@ -6,46 +6,103 @@ import java.awt.event.*;
 
 public class NewGUI extends JFrame {
 
-    private JButton button = new JButton("Далее");
-    private JLabel label1 = new JLabel("Введите ваше имя:");
-    private JLabel label2 = new JLabel("Кто вы?");
-    private JTextField input = new JTextField("", 5);
-    private JRadioButton radio1 = new JRadioButton("дебил");
-    private JRadioButton radio2 = new JRadioButton("не дебил");
+    private static final String NEXT = "Далее";
+    private static final String ENTER_YOUR_NAME = "Введите ваше имя:";
+    private static final String RESULT = "Результат";
+    private static final String WHAT = "Какой вы?";
+    private static final String WHO = "Кто вы?";
+    private static final String DEB = "дебил";
+    private static final String ADEB = "не дебил";
+    private static final String INTEL = "интеллектуал";
+    private static final String BEAU = "красивый ";
+    private static final String UGLY = "некрасивый ";
+    private static final String TEST = "Тест 'Познай себя'";
 
-   public NewGUI() {
-       super("Тест 'Познай себя'");
+    JPanel container;
+
+    private JButton button1 = new JButton(NEXT);
+    private JButton button2 = new JButton(RESULT);
+    private JLabel label1 = new JLabel(ENTER_YOUR_NAME);
+    private JLabel label2 = new JLabel(WHAT);
+    private JLabel label3 = new JLabel(WHO);
+
+    private JTextField input = new JTextField("", 5);
+    private JRadioButton radio1 = new JRadioButton(BEAU);
+    private JRadioButton radio2 = new JRadioButton(UGLY);
+    private JRadioButton radio3 = new JRadioButton(DEB);
+    private JRadioButton radio4 = new JRadioButton(ADEB);
+
+   private NewGUI() {
+       super(TEST);
        this.setBounds(100, 100, 500, 200);
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       Container container = this.getContentPane();
-        container.setLayout(new GridLayout(3,2,2,2));
+        container = (JPanel) this.getContentPane();
+        container.setLayout(new GridLayout(4,3,5,10));
         container.add(label1);
         container.add(input);
 
         container.add(label2);
-       ButtonGroup group = new ButtonGroup();
+      ButtonGroup group = new ButtonGroup();
        group.add(radio1);
        group.add(radio2);
         container.add(radio1);
        radio1.setSelected(true);
        container.add(radio2);
 
-       button.addActionListener(new ButtonEventListener());
-       container.add(button);
+
+       button1.addActionListener(new ButtonEventListenerNext());
+       container.add(button1);
+
+
+
+       /*if ((radio1.isSelected())) {
+           radio1.setSelected(true);
+       } else {
+           radio2.setSelected(true);
+       }
+
+       container.remove(radio1);
+       container.remove(radio2);
+       container.remove(label1);
+       container.remove(input);
+       container.remove(label2);
+
+       ButtonGroup group2 = new ButtonGroup();
+       group2.add(radio3);
+       group2.add(radio4);
+       container.add(radio3);
+       radio1.setSelected(true);
+       container.add(radio4);
+
+       if ((radio3.isSelected())) {
+           radio3.setSelected(true);
+       } else {
+           radio4.setSelected(true);
+       }*/
+
+       /*button2.addActionListener(new ButtonEventListenerRes());
+       container.add(button2);*/
     }
 
-    class ButtonEventListener implements ActionListener {
+    class ButtonEventListenerNext extends JFrame implements ActionListener  {
+       public void actionPerformed(ActionEvent d) {
+        next();
+       }
+    }
+
+    void next() {
+       container.remove(label1);
+    }
+
+    class ButtonEventListenerRes implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String message = "";
             message += "Поздравляю, " + input.getText() + ", вы ";
-            message +=  ((radio1.isSelected())
-                    ? "дебил" : "не дебил" );
+            message += ((radio3.isSelected()) ? BEAU : UGLY);
+            message += ((radio1.isSelected()) ? DEB : INTEL);
             message += " !";
-            JOptionPane.showMessageDialog(null,
-                    message,
-                    "Результат",
-                    JOptionPane.PLAIN_MESSAGE);
+            JOptionPane.showMessageDialog(null, message, RESULT, JOptionPane.PLAIN_MESSAGE);
         }
     }
 
---
+++ src/GraphicsUserInterface/TEST.java
@@ -0,0 +1,102 @@
+package GraphicsUserInterface;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class TEST {
+    JFrame frame1;
+    JPanel panel1;
+    JPanel panel2;
+
+    private static final String TEST = "Тест 'Познай себя'";
+    private static final String NEXT = "Далее";
+    private static final String RESULT = "Результат";
+    private static final String ENTER_YOUR_NAME = "Введите ваше имя:";
+    private static final String WHAT = "Какой вы?";
+    private static final String BEAU = "красивый ";
+    private static final String UGLY = "некрасивый ";
+    private static final String WHO = "Кто вы?";
+    private static final String DEB = "дебил";
+    private static final String ADEB = "не дебил";
+    private static final String INTEL = "интеллектуал";
+
+    private JTextField input = new JTextField("", 5);
+    private JLabel label1 = new JLabel(ENTER_YOUR_NAME);
+    private JLabel label2 = new JLabel(WHAT);
+    private JRadioButton radio1 = new JRadioButton(BEAU);
+    private JRadioButton radio2 = new JRadioButton(UGLY);
+    private JLabel label3 = new JLabel(WHO);
+    private JRadioButton radio3 = new JRadioButton(DEB);
+    private JRadioButton radio4 = new JRadioButton(ADEB);
+
+    public static void main(String[] args) {
+        TEST test = new TEST();
+        test.go();
+    }
+
+    public void go() {
+        frame1 = new JFrame(TEST);
+        frame1.setSize(500, 200);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button1 = new JButton(NEXT);
+        button1.addActionListener(new ButtonListenerNext());
+
+        panel1 = new JPanel();
+        panel1.add(label1);
+        panel1.add(input);
+        panel1.add(label2);
+        ButtonGroup group = new ButtonGroup();
+        group.add(radio1);
+        group.add(radio2);
+        panel1.add(radio1);
+        radio1.setSelected(true);
+        panel1.add(radio2);
+        panel1.add(button1);
+
+        frame1.add(panel1);
+        frame1.setVisible(true);
+    }
+
+    class ButtonListenerNext implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            go2();
+        }
+    }
+
+    public void go2() {
+        panel1.revalidate();
+        frame1.remove(panel1);
+
+        JButton button2 = new JButton(RESULT);
+        button2.addActionListener(new ButtonListenerRes());
+
+        panel2 = new JPanel();
+        panel2.add(label3);
+        ButtonGroup group = new ButtonGroup();
+        group.add(radio3);
+        group.add(radio4);
+        panel2.add(radio3);
+        radio3.setSelected(true);
+        panel2.add(radio4);
+        panel2.add(button2);
+
+        frame1.add(panel2);
+        frame1.repaint();
+    }
+
+    class ButtonListenerRes implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            go3();
+        }
+    }
+
+    public void go3() {
+        String message = "";
+        message += "Поздравляю, " + input.getText() + ", вы ";
+        message += ((radio1.isSelected()) ? BEAU : UGLY);
+        message += ((radio3.isSelected()) ? DEB : INTEL);
+        message += " !";
+        JOptionPane.showMessageDialog(null, message, RESULT, JOptionPane.PLAIN_MESSAGE);
+    }
+}
